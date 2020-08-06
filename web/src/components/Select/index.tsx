e9a176f345034be69a29ead09796bcc318cdeb1f
yuziem14 <yuri.fnunes@gmail.com>
2020-08-06 02:08:51 -0300

    refactor: abstracts form input blocks into components

---
+++ web/src/components/Select/index.tsx
@@ -0,0 +1,34 @@
+import React, { SelectHTMLAttributes } from 'react';
+
+import './styles.css';
+
+interface SelectOption {
+  value: string;
+  label: string;
+}
+
+interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
+  name: string;
+  label: string;
+  options: Array<SelectOption>;
+}
+
+const Select: React.FC<SelectProps> = ({ label, name, options, ...rest }) => {
+  return (
+    <div className='select-block'>
+      <label htmlFor={name}>{label}</label>
+      <select value='' id={name} {...rest}>
+        <option value='' disabled hidden>
+          Selecione uma opção
+        </option>
+        {options.map(option => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default Select;
