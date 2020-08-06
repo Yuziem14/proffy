bd542fbc373261c2474c4b2ac1f1d0870d20727c
yuziem14 <yuri.fnunes@gmail.com>
2020-08-06 20:28:34 -0300

    feat: create header component

---
+++ mobile/src/components/PageHeader/styles.ts
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#8257e5',
+    padding: 40,
+  },
+  topBar: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  title: {
+    maxWidth: 160,
+    fontFamily: 'Archivo_700Bold',
+    fontSize: 24,
+    lineHeight: 32,
+    color: '#FFF',
+    marginVertical: 40,
+  },
+});
+
+export default styles;
