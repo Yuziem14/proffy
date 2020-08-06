982d516dc774033ce8113c9f8d8eb3e5123f9985
yuziem14 <yuri.fnunes@gmail.com>
2020-08-06 20:30:06 -0300

    feat: create and style the favorites screen

---
+++ mobile/src/pages/Favorites/styles.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    backgroundColor: '#f0f0f7',
+  },
+});
+
+export default styles;
