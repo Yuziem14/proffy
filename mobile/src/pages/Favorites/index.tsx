b670dab2e3c674d569bdfbea3fec8c602e20309f
yuziem14 <yuri.fnunes@gmail.com>
2020-08-08 08:10:12 -0300

    feat: track favorite proffys with async storage and complete favorites page

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -1,16 +1,43 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, ScrollView } from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
 
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
 import PageHeader from '../../components/PageHeader';
 
 import styles from './styles';
+import AsyncStorage from '@react-native-community/async-storage';
 
-function Landing() {
+function Favorites() {
+  const [favorites, setFavorites] = useState([]);
+
+  function loadFavorites() {
+    AsyncStorage.getItem('favorites').then(response => {
+      if (response) {
+        const favoritedTeachers = JSON.parse(response);
+
+        setFavorites(favoritedTeachers);
+      }
+    });
+  }
+
+  useFocusEffect(() => {
+    loadFavorites();
+  });
+
   return (
     <View style={styles.container}>
       <PageHeader title='Meus proffys favoritos' />
+      <ScrollView
+        style={styles.teacherList}
+        contentContainerStyle={styles.teacherListContainer}
+      >
+        {favorites.map((teacher: Teacher) => {
+          return <TeacherItem key={teacher.id} teacher={teacher} favorited />;
+        })}
+      </ScrollView>
     </View>
   );
 }
 
-export default Landing;
+export default Favorites;
--- mobile/src/pages/Favorites/styles.ts
+++ mobile/src/pages/Favorites/styles.ts
@@ -2,8 +2,16 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     justifyContent: 'center',
     backgroundColor: '#f0f0f7',
+  },
+  teacherList: {
+    marginTop: -40,
+  },
+  teacherListContainer: {
+    paddingHorizontal: 16,
+    paddingBottom: 16,
   },
 });
 
