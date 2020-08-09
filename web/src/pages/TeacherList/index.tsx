70aeed13ef76325e0328499e7e30e0ab9e06e19f
yuziem14 <yuri.fnunes@gmail.com>
2020-08-09 04:17:05 -0300

    bugfix: change wrong label content

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,24 +1,24 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState, FormEvent } from "react";
 
-import api from '../../services/api';
+import api from "../../services/api";
 
-import Input from '../../components/Input';
-import Select from '../../components/Select';
-import PageHeader from '../../components/PageHeader';
-import TeacherItem, { Teacher } from '../../components/TeacherItem';
+import Input from "../../components/Input";
+import Select from "../../components/Select";
+import PageHeader from "../../components/PageHeader";
+import TeacherItem, { Teacher } from "../../components/TeacherItem";
 
-import './styles.css';
+import "./styles.css";
 
 function TeacherList() {
-  const [subject, setSubject] = useState('');
-  const [weekDay, setWeekDay] = useState('');
-  const [time, setTime] = useState('');
+  const [subject, setSubject] = useState("");
+  const [weekDay, setWeekDay] = useState("");
+  const [time, setTime] = useState("");
   const [teachers, setTeachers] = useState([]);
 
   async function searchTeachers(e: FormEvent) {
     e.preventDefault();
 
-    const { data } = await api.get('classes', {
+    const { data } = await api.get("classes", {
       params: {
         subject,
         week_day: weekDay,
@@ -30,57 +30,57 @@ function TeacherList() {
   }
 
   return (
-    <div id='page-teacher-list' className='container'>
-      <PageHeader title='Estes são os proffys disponíveis.'>
-        <form id='search-teachers' onSubmit={searchTeachers}>
+    <div id="page-teacher-list" className="container">
+      <PageHeader title="Estes são os proffys disponíveis.">
+        <form id="search-teachers" onSubmit={searchTeachers}>
           <Select
-            name='subject'
-            label='Matéria'
+            name="subject"
+            label="Matéria"
             value={subject}
-            onChange={e => {
+            onChange={(e) => {
               setSubject(e.target.value);
             }}
             options={[
-              { value: 'Artes', label: 'Artes-feira' },
-              { value: 'Biologia', label: 'Biologia' },
-              { value: 'Ciências', label: 'Ciências' },
-              { value: 'Educação Física', label: 'Educação Física' },
-              { value: 'Física', label: 'Física' },
-              { value: 'Geografia', label: 'Geografia' },
-              { value: 'História', label: 'História' },
-              { value: 'Matemática', label: 'Matemática' },
-              { value: 'Português', label: 'Português' },
-              { value: 'Química', label: 'Química' },
+              { value: "Artes", label: "Artes" },
+              { value: "Biologia", label: "Biologia" },
+              { value: "Ciências", label: "Ciências" },
+              { value: "Educação Física", label: "Educação Física" },
+              { value: "Física", label: "Física" },
+              { value: "Geografia", label: "Geografia" },
+              { value: "História", label: "História" },
+              { value: "Matemática", label: "Matemática" },
+              { value: "Português", label: "Português" },
+              { value: "Química", label: "Química" },
             ]}
           />
           <Select
-            name='week_day'
-            label='Dia da semana'
+            name="week_day"
+            label="Dia da semana"
             value={weekDay}
-            onChange={e => {
+            onChange={(e) => {
               setWeekDay(e.target.value);
             }}
             options={[
-              { value: '0', label: 'Domingo' },
-              { value: '1', label: 'Segunda-feira' },
-              { value: '2', label: 'Terça-feira' },
-              { value: '3', label: 'Quarta-feira' },
-              { value: '4', label: 'Quinta-feira' },
-              { value: '5', label: 'Sexta-feira' },
-              { value: '6', label: 'Sábado' },
+              { value: "0", label: "Domingo" },
+              { value: "1", label: "Segunda-feira" },
+              { value: "2", label: "Terça-feira" },
+              { value: "3", label: "Quarta-feira" },
+              { value: "4", label: "Quinta-feira" },
+              { value: "5", label: "Sexta-feira" },
+              { value: "6", label: "Sábado" },
             ]}
           />
           <Input
-            type='time'
-            name='time'
-            label='Hora'
+            type="time"
+            name="time"
+            label="Hora"
             value={time}
-            onChange={e => {
+            onChange={(e) => {
               setTime(e.target.value);
             }}
           />
 
-          <button type='submit'>Buscar</button>
+          <button type="submit">Buscar</button>
         </form>
       </PageHeader>
 
