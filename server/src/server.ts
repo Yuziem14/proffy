94cca947db038f2ae81501995c84f2e85dc1dd74
yuziem14 <yuri.fnunes@gmail.com>
2020-08-09 01:18:18 -0300

    chore: get env variables when server is fired

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,18 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 
 import routes from './routes';
 
+dotenv.config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-const PORT = 3333;
-app.listen(PORT, () => console.log('Listening on http://127.0.0.1:3333 ...'));
+const PORT = Number(process.env.PORT);
+const HOST = String(process.env.HOST);
+app.listen(PORT, HOST, () =>
+  console.log(`Listening on http://${HOST}:${PORT} ...`)
+);
