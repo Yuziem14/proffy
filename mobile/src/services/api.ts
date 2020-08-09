0521c8e7a3d087ddd51b97f14e81ec1111cf6330
yuziem14 <yuri.fnunes@gmail.com>
2020-08-09 03:40:41 -0300

    refactor: get the api url from env file

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import { API_URL } from '../config/api';
 
 const api = axios.create({
-  baseURL: 'http://192.168.1.188:3333',
+  baseURL: API_URL,
 });
 
 export default api;
