473370ef5ee82e3939b9cadf98a98a013b2670e5
yuziem14 <yuri.fnunes@gmail.com>
2020-08-13 01:07:12 -0300

    refactor: change subjects and weekdays data format

--- src/utils/format.js
+++ src/utils/format.js
@@ -1,29 +1,29 @@
 const subjects = [
-  'Artes',
-  'Biologia',
-  'Ciências',
-  'Educação Física',
-  'Física',
-  'Geografia',
-  'História',
-  'Matemática',
-  'Português',
-  'Química',
+  { value: 1, label: 'Artes' },
+  { value: 2, label: 'Biologia' },
+  { value: 3, label: 'Ciências' },
+  { value: 4, label: 'Educação Física' },
+  { value: 5, label: 'Física' },
+  { value: 6, label: 'Geografia' },
+  { value: 7, label: 'História' },
+  { value: 8, label: 'Matemática' },
+  { value: 9, label: 'Português' },
+  { value: 10, label: 'Química' },
 ];
 
 const weekdays = [
-  'Domingo',
-  'Segunda-feira',
-  'Terça-feira',
-  'Quarta-feira',
-  'Quinta-feira',
-  'Sexta-feira',
-  'Sábado',
+  { value: 0, label: 'Domingo' },
+  { value: 1, label: 'Segunda-feira' },
+  { value: 2, label: 'Terça-feira' },
+  { value: 3, label: 'Quarta-feira' },
+  { value: 4, label: 'Quinta-feira' },
+  { value: 5, label: 'Sexta-feira' },
+  { value: 6, label: 'Sábado' },
 ];
 
 function getSubject(subjectNumber) {
   const position = subjectNumber - 1;
-  return subjects[position];
+  return subjects[position].label;
 }
 
 function convertHoursToMinutes(time) {
