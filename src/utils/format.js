1753bcba44748a51414397e690418021473439f7
yuziem14 <yuri.fnunes@gmail.com>
2020-08-23 01:03:09 -0300

    refactor: add new functions and change weekday format

--- src/utils/format.js
+++ src/utils/format.js
@@ -1,3 +1,12 @@
+function Weekday(value, label) {
+  this.value = value;
+  this.label = label;
+}
+
+Weekday.prototype.short = function () {
+  return this.label.split('-')[0];
+};
+
 const subjects = [
   { value: 1, label: 'Artes' },
   { value: 2, label: 'Biologia' },
@@ -12,30 +21,72 @@ const subjects = [
 ];
 
 const weekdays = [
-  { value: 0, label: 'Domingo' },
-  { value: 1, label: 'Segunda-feira' },
-  { value: 2, label: 'Terça-feira' },
-  { value: 3, label: 'Quarta-feira' },
-  { value: 4, label: 'Quinta-feira' },
-  { value: 5, label: 'Sexta-feira' },
-  { value: 6, label: 'Sábado' },
+  new Weekday(0, 'Domingo'),
+  new Weekday(1, 'Segunda-feira'),
+  new Weekday(2, 'Terça-feira'),
+  new Weekday(3, 'Quarta-feira'),
+  new Weekday(4, 'Quinta-feira'),
+  new Weekday(5, 'Sexta-feira'),
+  new Weekday(6, 'Sábado'),
 ];
+
+function castToArray(data) {
+  const dataArray = Array.isArray(data) ? [...data] : [data];
+  return dataArray;
+}
 
 function getSubject(subjectNumber) {
   const position = subjectNumber - 1;
   return subjects[position].label;
 }
 
+function formatTime(time, truncate = false) {
+  if (time === 0 && truncate) {
+    return '';
+  }
+
+  if (time < 10) {
+    time = `0${time}`;
+  }
+
+  return time;
+}
+
+function formatCost(cost) {
+  const formatter = new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  });
+
+  return formatter.format(cost);
+}
+
 function convertHoursToMinutes(time) {
+  const regex = new RegExp(/(\d{2}):(\d{2})/);
+
+  if (typeof time !== 'string' || !regex.test(time)) {
+    return time;
+  }
+
   const [hour, minutes] = time.split(':');
   const totalMinutes = Number(hour * 60 + Number(minutes));
 
   return totalMinutes;
 }
 
+function convertMinutesToHours(time) {
+  const hours = Math.floor(time / 60);
+  const minutes = time % 60;
+
+  return `${formatTime(hours)}h${formatTime(minutes, true)}`;
+}
+
 module.exports = {
   subjects,
   weekdays,
   getSubject,
+  castToArray,
   convertHoursToMinutes,
+  convertMinutesToHours,
+  formatCost,
 };
